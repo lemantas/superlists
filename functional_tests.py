51fd5b27661f9ca678f74d60bf9cd8b123ee16f0
Mantas Levinas <mantas.levinas@gmail.com>
2017-07-16 22:18:06 +0300

    Functional test now checks we can input to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -10,10 +11,35 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 		
-	def test_can_start(self):
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# Enter the homepage
 		self.browser.get('http://localhost:8000')
 		
+		# Does the page title and header mention to-do lsits?
 		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+		
+		# Eter the To-Do item
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
+		
+		# Type in text
+		inputbox.send_keys('Buy peacock feathers')
+		
+		# After hitting enter the page updates with a new item in a to-do list
+		inputbox.send_keys(Keys.ENTER)
+		
+		table = self.browser.find_elements_by_tag_name('tr')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
+		
+		# Enter a second item
 		self.fail('Finish the test!')
 
 if __name__ == '__main__':
