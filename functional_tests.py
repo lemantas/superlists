c44962d06fbb800f60632cc099110d9a46940b73
Mantas Levinas <mantas.levinas@gmail.com>
2017-07-20 22:06:00 +0300

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,7 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.quit()
 		
 	def check_for_row_in_list_table(self, row_text):
+		self.browser.get('http://localhost:8000')
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertIn(row_text, [row.text for row in rows])
@@ -48,7 +49,7 @@ class NewVisitorTest(unittest.TestCase):
 		
 		# Assert for two items in a list table
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.check_for_row_in_list_table('2: use peacock feathers to make a fly')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		
 		# proceed with tests here
