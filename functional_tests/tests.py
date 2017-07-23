feb70b852c42d23fdb295333cb558216d17d975f
Mantas Levinas <mantas.levinas@gmail.com>
2017-07-23 20:04:35 +0300

    Brand new commit after introducing Francis

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -39,8 +39,8 @@ class NewVisitorTest(LiveServerTestCase):
 		
 		# After hitting enter the page updates with a new item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
-		
-		# Assert for one item in a list table
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		
 		# Type in some more text for the second element
@@ -53,5 +53,27 @@ class NewVisitorTest(LiveServerTestCase):
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		
-		# proceed with tests here
-		self.fail('Finish the test!')
+		## Now it's Francis' turn. He revisits site and makes suere  there is no trace of Edith's #
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.fing_element_by_tag_name('body')
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+		
+		# Francis adds a new item
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+		
+		# Franics gets his own unique URL
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list-url, '/lists/.+')
+		self.assertNotEqual(francis_lsit_url, edith_list_url)
+		
+		# Again, there is no trace of Edith's list
+		page_text = self.browser.find_element._by_tag_name('body')
+		self.assertNotIn('Buy milk, page_text')
+		
+		# Satisfied, they both go back to sleep together
+		self.assertFail('Need to implement more tests')
