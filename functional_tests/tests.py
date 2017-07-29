ba5fb4628d938f36f24511daf962a3be852626eb
Mantas Levinas <mantas.levinas@gmail.com>
2017-07-29 15:24:03 +0300

    refactor templates to use a base templates and add bootstrap library

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -30,7 +30,7 @@ class NewVisitorTest(LiveServerTestCase):
                 time.sleep(0.5)
 
 
-    def atest_can_start_a_list_for_one_user(self):
+    def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get(self.live_server_url)
@@ -70,7 +70,7 @@ class NewVisitorTest(LiveServerTestCase):
         # Satisfied, she goes back to sleep
 
 
-    def atest_multiple_users_can_start_lists_at_different_urls(self):
+    def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith start a new todo list
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
