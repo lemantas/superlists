d31826c91836c851de621935484ea94766899338
Mantas Levinas <mantas.levinas@gmail.com>
2017-08-13 13:54:43 +0300

    Add a fabfile for automated deploys

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,10 +1,11 @@
-from fabric.contrib.files import append, exists, sed from fabric.api import env, local, run
+from fabric.contrib.files import append, exists, sed 
+from fabric.api import env, local, run
 import random
 
 REPO_URL = 'https://github.com/lemantas/superlists'  
 
 def deploy():
-    site_folder = '/home/%s/sites/%s' % (env.user, env.host)  
+    site_folder = '/home/%s/sites/%s' % (env.user, env.host)
     source_folder = site_folder + '/source'
     _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
@@ -19,9 +20,9 @@ def _create_directory_structure_if_necessary(site_folder):
 
 def _get_latest_source(source_folder):
     if exists(source_folder + '/.git'):
-        run('cd %s && git fetch' % (source_folder,)
+        run('cd %s && git fetch' % (source_folder,))
     else:
-        run('git clone %s %s' % (REPO_URL, source_folder)
+        run('git clone %s %s' % (REPO_URL, source_folder))
     current_commit = local("git log -n 1 --format=%H", capture=True) 
     run('cd %s && git reset --hard %s' % (source_folder, current_commit))
 
