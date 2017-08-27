cd4168f969db35c1898d545153fa439edf9b4d26
Mantas Levinas <mantas.levinas@gmail.com>
2017-08-27 12:32:05 +0300

    use form in all views, back to working state

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,18 +7,26 @@ class ItemValidationTest(FunctionalTest):
 	def test_cannot_add_empty_list_items(self):
 		# Blah blah - long story: Edith tries adding blank items
 		self.browser.get(self.server_url)
-		self.get_item_input_box().send_keys('\n')
+		inputbox = self.get_item_input_box()
+		inputbox.send_keys('\n')
+		inputbox.submit()
+		import time
+		time.sleep(1)
 
 		# Test error message for imputing blank items
 		error = self.browser.find_element_by_css_selector('.has-error')
 		self.assertEqual(error.text, "You can't have an empty list item")
 
 		# Now with text
-		self.get_item_input_box().send_keys('Buy milk\n')
+		inputbox = self.get_item_input_box()
+		inputbox.send_keys('Buy milk\n')
+		inputbox.submit()
 		self.wait_for_row_in_list_table('1: Buy milk')
 
 		# What a pervert that Edith, she tries empty item again!
-		self.get_item_input_box().send_keys('\n')
+		inputbox = self.get_item_input_box()
+		inputbox.send_keys('\n')
+		inputbox.submit()
 
 		# She receives a similar warning
 		self.wait_for_row_in_list_table('1: Buy milk')
@@ -26,9 +34,9 @@ class ItemValidationTest(FunctionalTest):
 		self.assertEqual(error.text, "You can't have an empty list item")
 
 		# And she can correct it by filling some text in
-		self.get_item_input_box().send_keys('Make tea\n')
+		inputbox = self.get_item_input_box()
+		inputbox.send_keys('Make tea\n')
+		inputbox.submit()
 		self.wait_for_row_in_list_table('1: Buy milk') 
 		self.wait_for_row_in_list_table('2: Make tea')
-
-		self.fail('write me!')
 
